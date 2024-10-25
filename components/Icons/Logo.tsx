311cd4aae0463887d183567ce4a7f9a6cf16b03a
Ben Hadfield <benjohnhadfield@gmail.com>
2024-10-25 21:56:01 +0100

    bump typescript to latest

--- components/Icons/Logo.tsx
+++ components/Icons/Logo.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
-export default function Logo () {
+export default function Logo ({ width }: { width: number }) {
   return (
     <Image
       priority
       src='/logo.png'
       alt='BryFly logo'
-      width={230}
+      width={width}
       height={50}
     />
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -46,22 +46,29 @@ export default function Home ({ images }: InferGetStaticPropsType<typeof getStat
         )}
         <div className='columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4'>
           <div className='after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0'>
-            <Logo />
+            <Logo width={286} />
             <h1 className='mt-8 mb-4 text-base font-bold tracking-widest'>
               &quot;browse me disco balls!&quot;
             </h1>
-            <p className='max-w-[40ch] text-white/75 sm:max-w-[32ch]'>
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
-            </p>
-            <a
-              className='pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4'
-              href='https://vercel.com/new/clone?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-cloudinary&project-name=nextjs-image-gallery&repository-name=with-cloudinary&env=NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET,CLOUDINARY_FOLDER&envDescription=API%20Keys%20from%20Cloudinary%20needed%20to%20run%20this%20application'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Clone and Deploy
-            </a>
+            {/*<p className='max-w-[40ch] text-white/75 sm:max-w-[32ch]'>*/}
+            {/*  Bryony's balls blah blah blahblahblah blahblah b-blah blah.*/}
+            {/*</p>*/}
+            <div className='z-10 mt-6'>
+              <a
+                className='pointer mr-2 rounded-lg border border-white bg-transparent px-3 py-2 text-sm font-medium text-white transition hover:bg-white/10 hover:text-white md:mt-4'
+                href='https://www.instagram.com/bryfly2000/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Instagram
+              </a>
+              <a
+                className='pointer rounded-lg border border-white bg-gray-100 px-3 py-2 text-sm font-bold text-black transition hover:bg-white/10 hover:text-white md:mt-4'
+                href='/contact'
+              >
+                I'm interested!
+              </a>
+            </div>
           </div>
           {images.map(({ index, public_id, format, blurDataUrl }) => {
             return (
@@ -92,35 +99,8 @@ export default function Home ({ images }: InferGetStaticPropsType<typeof getStat
           })}
         </div>
       </main>
-      <footer className='p-6 text-center text-white/80 sm:p-12'>
-        Thank you to{' '}
-        <a
-          href='https://edelsonphotography.com/'
-          target='_blank'
-          className='font-semibold hover:text-white'
-          rel='noreferrer'
-        >
-          Josh Edelson
-        </a>
-        ,{' '}
-        <a
-          href='https://www.newrevmedia.com/'
-          target='_blank'
-          className='font-semibold hover:text-white'
-          rel='noreferrer'
-        >
-          Jenny Morgan
-        </a>
-        , and{' '}
-        <a
-          href='https://www.garysextonphotography.com/'
-          target='_blank'
-          className='font-semibold hover:text-white'
-          rel='noreferrer'
-        >
-          Gary Sexton
-        </a>{' '}
-        for the pictures.
+      <footer className='p-6 text-center text-white/40 sm:p-12'>
+        Made by Moose.
       </footer>
     </>
   )
