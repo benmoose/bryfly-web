9f514589aa10d29230caf0ed283449d2926978cb
Ben Hadfield <benjohnhadfield@gmail.com>
2025-02-05 02:28:56 +0000

    dep update

--- scripts/update-node.sh
+++ scripts/update-node.sh
@@ -5,22 +5,33 @@ IFS=$'\n\t'
 nvm_dir=${NVM_DIR:?"nvm is not installed."}
 [[ -f "$nvm_dir/nvm.sh" ]] && source "$nvm_dir/nvm.sh"
 
-semver="20"
-app_dir=${0%%/*}
-project_v=$(cat "$app_dir/.nvmrc")
-latest_v=$(nvm version-remote --lts "$semver")
+# Style
+bold='\033[0;1m'
+dim='\033[0;2m'
+normal='\033[0;0m'
+# Colour
+green='\033[0;32m'
+clear='\033[0m'
 
-[[ "$project_v" == "$latest_v" ]] && exit
+app_dir=${0%%/*}
+current_node=$(cat "$app_dir/.nvmrc")
+engine=$(pnpm pkg get engines.node | tr -cd '[:digit:]')
+latest_node=$(nvm version-remote --lts "$engine")
+[[ "$current_node" == "$latest_node" ]] && exit
+
+echo -e "Latest:  ${bold}$latest_node${normal} ${dim}(constraint '$engine')${normal}"
+echo -e "Project: ${bold}$current_node${normal}"
 
 if [[ $# -eq 0 ]]; then
-  echo "Project uses Node $project_v. Please update to Node $latest_v." >&2
+  echo "Please update project to use node $latest_node" >&2
   exit 1
 elif [[ "${1-}" == "-w" || "${1-}" == "--write" ]]; then
-  nvm install --lts --reinstall-packages-from=current "$latest_v" && \
-    nvm use --silent "$latest_v" && \
-    nvm current > "$app_dir/.nvmrc" && \
-    echo "Project engine updated to Node $latest_v!"
+  echo "$latest_node" > "$app_dir/.nvmrc" && \
+    echo -e "${green}Project updated!${clear}" && \
+    echo ""
+  nvm install --latest-npm --reinstall-packages-from=current "$latest_node"
   exit $?
 else
   echo "usage: $0 [--write, -w]"
 fi
+
