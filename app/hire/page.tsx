94574baa4d2b8f114bb9a0e28ecbf5c3f5e1a607
Ben Hadfield <benjohnhadfield@gmail.com>
2025-04-27 14:43:36 +0100

    hire details page

--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -17,9 +17,9 @@ export default function Page() {
 
       <main className="@container grid gap-3 columns-1 @xl:columns-2 @4xl:columns-3">
         <div className="columns-1 @xl:columns-2 @4xl:columns-3 *:mb-3">
-          <HireLink href="/" image="" text="Foo bar fas" title="Flange" />
+          <HireLink href="/flange" image="" text="Foo bar fas" title="Flange" />
           <HireLink
-            href="/"
+            href="/disco-dick"
             image=""
             text="Foo bar fas"
             title="The Disco Dick"
