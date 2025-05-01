da8f2a67f1f08edce78c87ac5288b920163dea59
Ben Hadfield <benjohnhadfield@gmail.com>
2025-05-01 16:28:51 +0100

    hiring page copy

--- app/hire/page.tsx
+++ app/hire/page.tsx
@@ -12,7 +12,18 @@ export default function Page() {
     <div className="w-full max-w-[1920px] mx-auto p-4">
       <H1 className="text-center mb-6">Available for hire</H1>
       <div className="w-full max-w-2xl mx-auto mb-20 px-3">
-        <P>{pageText}</P>
+        <P>
+          BryFly's unique reflective artwork has delighted audiences at some of
+          the UK’s best festivals and events – from Noisily, to Homobloc and
+          Wildwood Disco. Her designs have also featured in music videos and in
+          the Disney series ‘Extraordinary’.
+        </P>
+        <P>
+          Hiring costs depend on the individual requirements of the event or
+          production, so please email for more information. All pieces will be
+          packaged securely for transportation. Transportation arrangements are
+          not included.
+        </P>
       </div>
 
       <main className="@container grid gap-3 columns-1 @xl:columns-2 @4xl:columns-3">
